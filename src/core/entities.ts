0d3dd02ef7d2be4b1661b24faae09fe9005aea6c
biffyclyro <joao-senna1@hotmail.com>
2022-10-02 16:09:59 -0300

    ajustando a forma de criar a colisão

--- src/core/entities.ts
+++ src/core/entities.ts
@@ -16,9 +16,7 @@ export enum Direction {
 }
 
 interface Sensors{
-	bottom: Physics.Matter.Sprite
-	left?: Physics.Matter.Sprite
-	right?: Physics.Matter.Sprite
+	bottom: MatterJS.BodyType
 }
 
 export interface Item extends Physics.Matter.Image{
@@ -70,26 +68,17 @@ export class Player extends SpriteEntity {
 	inventory: Item[] | undefined
 
 	setSprite(scene: Scene, { x, y, width, height, scale }: Entity) {
-		//super.setSprite(scene, {x:x, y:y, width:width, height: height, scale: scale})
-		this.sensors = {
-			bottom: scene.matter.add.sprite(x, y + height, 'bottom-sensor')
-		}
-		this.sensors.bottom.setRectangle(width, 0)
-
 		const Bodies = scene.matter.bodies 
-		const body = Bodies.rectangle(x, y, width, height)
-		const teste = Bodies.rectangle(x, y + width/2, width, 1)
-		teste.isSensor = true
-
-	
-
+		this.sensors = {
+			bottom: Bodies.rectangle(12, 20, width, 1,{isSensor: true})
+		}
+		const body = Bodies.rectangle(12, 13, width, height)
 		const compoundBody = scene.matter.body.create({
-			parts: [body, teste],
+			parts: [body, this.sensors.bottom],
 		})
-		this.sprite = scene.matter.add.sprite(0, 0, this.baseTexture, 0)	
+		this.sprite = scene.matter.add.sprite(x, y, this.baseTexture, 0)	
 		this.sprite.setExistingBody(compoundBody)
 		this.sprite.setScale(scale)
 		this.sprite.setFixedRotation()
-
 	}
 }
